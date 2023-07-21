c39e6cca551662a254374e7a8345e86c3a2ae583
lucas.oliveira <lucasaugustooliveira75@gmail.com>
2023-07-21 16:31:05 -0300

    Trabalhando com alerta de erros no Thymeleaf

--- Java_Spring_Thymeleaf/mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ Java_Spring_Thymeleaf/mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,11 +1,12 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
@@ -19,13 +20,17 @@ public class PedidoController {
 	
 	
 	@GetMapping("formulario")
-	public String formulario() {
+	public String formulario(@Valid RequisicaoNovoPedido requisicaoNovoPedido) {
 		return "pedidos/formulario";
 	}
 	
 	@PostMapping("novo")
-	public String novo(RequisicaoNovoPedido dados) {
-		Pedido pedido = dados.toPedido();
+	public String novo(@Valid RequisicaoNovoPedido requisicaoNovoPedido, BindingResult resultado) {
+		if(resultado.hasErrors()) {
+			return "pedidos/formulario";
+		}
+		
+		Pedido pedido = requisicaoNovoPedido.toPedido();
 		
 		repository.save(pedido);
 		
