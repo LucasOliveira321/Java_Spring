f1fe6bcc45301d33e3a4821c0e0421b0ddf51fc9
lucas.oliveira <lucasaugustooliveira75@gmail.com>
2023-07-24 17:05:02 -0300

    Implementando a interface CRUDRepository

--- Java_Spring_JPA/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Java_Spring_JPA/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.orm.Cargo;
+import com.example.demo.repository.CargoRepository;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner{
+	
+	
+	private	final CargoRepository repository;
+	
+	public DemoApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		
+		repository.save(cargo);
 	}
 
 }
---
+++ Java_Spring_JPA/demo/src/main/java/com/example/demo/repository/JpaExemplo.java
@@ -0,0 +1,41 @@
+package com.example.demo.repository;
+
+import com.example.demo.orm.Cargo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaExemplo {
+	
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	
+	public JpaExemplo() {
+		emf = Persistence.createEntityManagerFactory("jpa");
+		em = emf.createEntityManager();
+	}
+	
+	public void save(Cargo cargo) {
+		em.getTransaction().begin();
+		em.persist(cargo);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public Cargo findById(Integer id) {
+		em.getTransaction().begin();
+		Cargo cargo = em.find(Cargo.class, id);
+		em.getTransaction().commit();
+		em.close();
+		return cargo;
+	}
+	
+	public void deleteById(Integer id) {
+		Cargo cargo = em.find(Cargo.class, id);
+		
+		em.getTransaction().begin();
+		em.remove(cargo);
+		em.getTransaction().commit();
+		
+	}
+}
