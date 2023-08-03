9badf1d4aad70e225bdccc6bff5e7d7a394fd62b
lucas.oliveira <lucasaugustooliveira75@gmail.com>
2023-08-03 08:15:27 -0300

    Criando mais Query's diferentes e concluindo o curso de Hibernate

--- Java_Spring_Hibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ Java_Spring_Hibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja.dao;
 
+import java.math.BigDecimal;
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -24,5 +26,20 @@ public class ProdutoDao {
 	public List<Produto> buscarTodos(){
 		String jpql = "SELECT p FROM Produto p";
 		return em.createQuery(jpql, Produto.class).getResultList();	
+	}
+	
+	public List<Produto> buscarPorNome(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, Produto.class).setParameter("nome",nome).getResultList();	
+	}
+	
+	public List<Produto> buscarPorNomeDaCategoria(String categoria){
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :categoria";
+		return em.createQuery(jpql, Produto.class).setParameter("categoria",categoria).getResultList();	
+	}
+	
+	public BigDecimal buscarPorPrecoDoProdutoComNome(String nome){
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, BigDecimal.class).setParameter("nome",nome).getSingleResult();
 	}
 }
--- Java_Spring_Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ Java_Spring_Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -2,9 +2,7 @@ package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
 import java.util.List;
-
 import javax.persistence.EntityManager;
-
 import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
