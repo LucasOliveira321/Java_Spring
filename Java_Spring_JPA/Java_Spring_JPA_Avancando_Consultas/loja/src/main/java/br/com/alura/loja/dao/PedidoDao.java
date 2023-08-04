a9245db1f1581c7749afa6655cca2b0a749ea748
lucas.oliveira <lucasaugustooliveira75@gmail.com>
2023-08-04 11:35:08 -0300

    Criando método de busca com critérios opcionais e utilizando Critéria

--- Java_Spring_JPA/Java_Spring_JPA_Avancando_Consultas/loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ Java_Spring_JPA/Java_Spring_JPA_Avancando_Consultas/loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -1,11 +1,18 @@
 package br.com.alura.loja.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.alura.loja.modelo.Pedido;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 public class PedidoDao {
@@ -53,4 +60,61 @@ public class PedidoDao {
 		return em.createQuery("SELECT p FROM Pedido p JOIN FETCH p.cliente WHERE p.id = :id", Pedido.class).setParameter("id", id).getSingleResult();
 	}
 	
+	
+	// O método abaixo é uma busca com os parâmetros opcionais
+	// e pode ser utilizado um ou todos o parâmetros do método
+	// que será adicionado na query conforme os parâmetros atribuidos.
+	// Quando chamar o método os parâmetros que não for utilizar
+	// é preciso colocar null.
+	public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro){
+		// O where 1=1 é feito para dar true e facilitar os próxmos if's
+		// que estão verificando os parametros passado no método
+		String jpql = "SELECT p FROM Produto p WHERE 1=1 ";
+		if(nome != null && !nome.trim().isEmpty()) {
+			jpql = " AND p.nome = :nome ";
+		}
+		if(preco != null) {
+			jpql = " AND p.preco = :preco ";
+		}
+		if(dataCadastro != null) {
+			jpql = " AND p.dataCadastro = :dataCadastro ";
+		}
+		
+		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+		if(nome != null && !nome.trim().isEmpty()) {
+			query.setParameter("nome", nome);
+		}
+		if(preco != null) {
+			query.setParameter("preco", preco);
+		}
+		if(dataCadastro != null) {
+			query.setParameter("dataCadastro", dataCadastro);
+		}
+		
+		return query.getResultList();
+	}
+	
+	// É uma outra maneira de realizar o método com parâmetros
+	// opcionais, mas utilizando o Critéria que é uma classe
+	// que auxilia na construção do método.
+	public List<Produto> buscarPorParametrosComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro){
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
+		Root<Produto> from = query.from(Produto.class);
+		
+		Predicate filtros = builder.and();
+		
+		if(nome != null && !nome.trim().isEmpty()) {
+			filtros = builder.and(filtros, builder.equal(from.get("nome"), nome));
+		}
+		if(preco != null) {
+			filtros = builder.and(filtros, builder.equal(from.get("preco"), preco));
+		}
+		if(dataCadastro != null) {
+			filtros = builder.and(filtros, builder.equal(from.get("dataCadastro"), dataCadastro));
+		}
+		query.where(filtros);
+		
+		return em.createQuery(query).getResultList();
+	}
 }
